fa4b2ca9582e1ef39a7544b3ff045f9c7161ecc9
ashishsoniii <ashishsoni2002@gmail.com>
2024-04-25 16:27:01 +0530

    Add Plan form Added

--- Frontend/src/sections/plan/view/plan-view.jsx
+++ Frontend/src/sections/plan/view/plan-view.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
+import { Grid } from '@mui/material';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
 import Table from '@mui/material/Table';
@@ -16,17 +17,21 @@ import TablePagination from '@mui/material/TablePagination';
 import Iconify from 'src/components/iconify';
 import Scrollbar from 'src/components/scrollbar';
 
+import AddPlanForm from './form-new-plan';
 import TableNoData from '../table-no-data';
 import UserTableRow from '../plan-table-row';
 import UserTableHead from '../plan-table-head';
 import TableEmptyRows from '../table-empty-rows';
 import UserTableToolbar from '../plan-table-toolbar';
+import AppWidgetSummary from '../app-widget-summary';
 import { emptyRows, applyFilter, getComparator } from '../utils';
 
 // ----------------------------------------------------------------------
 
 export default function PlanPage() {
   const [page, setPage] = useState(0);
+
+  const [clickedTitle, setClickedTitle] = useState('All Plans');
 
   const [plans, setPlans] = useState([]);
 
@@ -39,6 +44,10 @@ export default function PlanPage() {
   const [filterName, setFilterName] = useState('');
 
   const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleCardClick = (title) => {
+    setClickedTitle(title);
+  };
 
   const handleSort = (event, id) => {
     const isAsc = orderBy === id && order === 'asc';
@@ -60,7 +69,7 @@ export default function PlanPage() {
 
   useEffect(() => {
     fetchPlans(); // Fetch plans when the component mounts
-  }, []); // Empty dependency array ensures this effect runs only once
+  }, [clickedTitle]); // Empty dependency array ensures this effect runs only once
 
   const handleSelectAllClick = (event) => {
     if (event.target.checked) {
@@ -121,71 +130,111 @@ export default function PlanPage() {
         </Button>
       </Stack>
 
+      <Grid container spacing={3} m={5} gap={4}>
+        <Grid xs={12} sm={6} md={3}>
+          <AppWidgetSummary
+            total="All Plans"
+            color="success"
+            sx={{
+              cursor: 'pointer',
+              '&:hover': {
+                cursor: 'pointer', // Change cursor to pointer on hover
+              },
+            }}
+            onClick={() => handleCardClick('All Plans')}
+            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
+          />
+        </Grid>
+        <Grid xs={12} sm={6} md={3}>
+          <AppWidgetSummary
+            total="New Plan"
+            color="info"
+            sx={{
+              cursor: 'pointer',
+              '&:hover': {
+                cursor: 'pointer', // Change cursor to pointer on hover
+              },
+            }}
+            onClick={() => handleCardClick('New Plan')}
+            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />}
+          />
+        </Grid>
+      </Grid>
+
       <Card>
-        <UserTableToolbar
-          numSelected={selected.length}
-          filterName={filterName}
-          onFilterName={handleFilterByName}
-        />
+        {clickedTitle === 'All Plans' && (
+          <>
+            <UserTableToolbar
+              numSelected={selected.length}
+              filterName={filterName}
+              onFilterName={handleFilterByName}
+            />
+            <Scrollbar>
+              <TableContainer sx={{ overflow: 'unset' }}>
+                <Table sx={{ minWidth: 800 }}>
+                  <UserTableHead
+                    order={order}
+                    orderBy={orderBy}
+                    rowCount={plans.length}
+                    numSelected={selected.length}
+                    onRequestSort={handleSort}
+                    onSelectAllClick={handleSelectAllClick}
+                    headLabel={[
+                      { id: '_id', label: 'Plan Id' },
+                      { id: 'name', label: 'Plan Name' },
+                      { id: 'duration', label: 'Plan Duration (in Months)' },
+                      { id: 'description', label: 'Description' },
+                      { id: 'price', label: 'Price', align: 'center' },
+                      { id: 'isActive', label: 'Status' },
+                      { id: '' },
+                    ]}
+                  />
+                  <TableBody>
+                    {dataFiltered
+                      .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                      .map((row) => (
+                        <UserTableRow
+                          key={row._id}
+                          id={row._id}
+                          name={row.name}
+                          duration={row.duration}
+                          description={row.description}
+                          price={row.price}
+                          status={row.isActive ? 'active' : 'inactive'}
+                          avatarUrl={row.avatarUrl}
+                          selected={selected.indexOf(row.name) !== -1}
+                          handleClick={(event) => handleClick(event, row.name)}
+                        />
+                      ))}
 
-        <Scrollbar>
-          <TableContainer sx={{ overflow: 'unset' }}>
-            <Table sx={{ minWidth: 800 }}>
-              <UserTableHead
-                order={order}
-                orderBy={orderBy}
-                rowCount={plans.length}
-                numSelected={selected.length}
-                onRequestSort={handleSort}
-                onSelectAllClick={handleSelectAllClick}
-                headLabel={[
-                  { id: '_id', label: 'Plan Id' },
-                  { id: 'name', label: 'Plan Name' },
-                  { id: 'duration', label: 'Plan Duration (in Months)' },
-                  { id: 'description', label: 'Description' },
-                  { id: 'price', label: 'Price', align: 'center' },
-                  { id: 'isActive', label: 'Status' },
-                  { id: '' },
-                ]}
-              />
-              <TableBody>
-                {dataFiltered
-                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                  .map((row) => (
-                    <UserTableRow
-                      key={row._id}
-                      id={row._id}
-                      name={row.name}
-                      duration={row.duration}
-                      description={row.description}
-                      price={row.price}
-                      status={row.isActive ? 'active' : 'inactive'}
-                      avatarUrl={row.avatarUrl}
-                      selected={selected.indexOf(row.name) !== -1}
-                      handleClick={(event) => handleClick(event, row.name)}
+                    <TableEmptyRows
+                      height={77}
+                      emptyRows={emptyRows(page, rowsPerPage, plans.length)}
                     />
-                  ))}
 
-                <TableEmptyRows
-                  height={77}
-                  emptyRows={emptyRows(page, rowsPerPage, plans.length)}
-                />
+                    {notFound && <TableNoData query={filterName} />}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Scrollbar>
 
-                {notFound && <TableNoData query={filterName} />}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Scrollbar>
-
-        <TablePagination
-          page={page}
-          component="div"
-          count={plans.length}
-          rowsPerPage={rowsPerPage}
-          onPageChange={handleChangePage}
-          rowsPerPageOptions={[5, 10, 25]}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
+            <TablePagination
+              page={page}
+              component="div"
+              count={plans.length}
+              rowsPerPage={rowsPerPage}
+              onPageChange={handleChangePage}
+              rowsPerPageOptions={[5, 10, 25]}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </>
+        )}{' '}
+        {clickedTitle === 'New Plan' && (
+          <>
+            {/* add new pla n here */}
+            <AddPlanForm />
+          </>
+        )}{' '}
       </Card>
     </Container>
   );
